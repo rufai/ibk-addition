403705300044d681f82b520c0d705e10915f90cd
Rufai Mustapha <etop94@gmail.com>
2022-02-20 18:56:14 +0100

    Update export modules

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+/*!
+ * ibk_addition
+ * Copyright(c) 2022 Professor IBK
+ * Copyright(c) 2022 Mustapha Rufai
+ * MIT Licensed
+ */
+
+'use strict'
+module.exports = ibk_addition;
+
 function ibk_addition(a,b){
     return a + b
 }
